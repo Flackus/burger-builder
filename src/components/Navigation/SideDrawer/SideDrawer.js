ba30a14a2b0ab3b1556a796e724ea3355286192e
Alexey Rybakov <alexrybakov@yandex.ru>
2020-08-04 01:02:32 +0300

    158. Useful Resources & Links

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux/Aux';
+
+const sideDrawer = (props) => {
+    const attachedClasses = [classes.SideDrawer];
+
+    if (props.open) {
+        attachedClasses.push(classes.Open);
+    } else {
+        attachedClasses.push(classes.Closed);
+    }
+
+    return (
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
+    );
+}
+
+export default sideDrawer;
