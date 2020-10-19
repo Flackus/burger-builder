cb905bee74bd5f22ed2d3be4ddcad13100829a0c
Alexey Rybakov <aleksei.rybakov@bolt.eu>
2020-10-19 10:42:16 +0300

    SAVEPOINT

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
-        type: actionTypes.AUTH_START,
-        authData
+        type: actionTypes.AUTH_SUCCESS,
+        token,
+        userId
     };
 };
 
@@ -19,6 +20,20 @@ export const authFail = (error) => {
     return {
         type: actionTypes.AUTH_FAIL,
         error
+    };
+};
+
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
     };
 };
 
@@ -34,15 +49,16 @@ export const auth = (email, password, isSignup) => {
         };
         let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp`;
         if (!isSignup) {
-            url = `https://identitytoolkit.googleapis.com/v1/accounts:verifyPassword`;
+            url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword`;
         }
         url += `?key=${AUTH_API_KEY}`;
         axios.post(url, authData, {headers: {}})
             .then(response => {
-                dispatch(authSuccess(response.data));
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(error => {
-                dispatch(authFail(error));
+                dispatch(authFail(error.response.data.error));
             });
     };
 };
