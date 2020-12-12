9a67da6f94fa90fd084ac511bc0e3c58ff662fad
Alexey Rybakov <alexrybakov@yandex.ru>
2020-12-12 21:32:04 +0200

    363. Useful Resources & Links

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,37 @@
+import reducer from './auth';
+import * as actionTypes from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        });
+    });
+
+    it('should store the token upon login', () => {
+        expect(
+            reducer({
+                token: null,
+                userId: null,
+                error: null,
+                loading: false,
+                authRedirectPath: '/'
+            },
+            {
+                type: actionTypes.AUTH_SUCCESS,
+                token: "test-token",
+                userId: "test-id"
+            })
+        ).toEqual({
+            token: "test-token",
+            userId: "test-id",
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        });
+    });
+});
